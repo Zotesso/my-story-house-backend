aadc75df086708b77e3506c620d49f53e3a5f722
Pedro Zotesso <siriothart@gmail.com>
2022-09-18 00:06:30 -0300

    Graphql - Removed express dependancy and installed apollo

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import 'reflect-metadata';
+
+import path from "path";
+import { buildSchema } from "type-graphql";
+import { ApolloServer } from "apollo-server";
+import { LibraryResolver } from "./resolvers/LibraryResolver";
+
+async function main() {
+  const schema = await buildSchema({
+    resolvers: [
+      LibraryResolver,
+    ],
+    emitSchemaFile: path.resolve(__dirname, 'schema.gql'),
+  })
+
+  const server = new ApolloServer({
+    schema,
+  })
+
+  const { url } = await server.listen();
+  console.log(`Server running on ${url}`);
+}
+
+main();
