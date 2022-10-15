fda7309f6b5de700239dfc3ea441a84eb7aa37dc
Pedro Zotesso <siriothart@gmail.com>
2022-10-15 13:12:38 -0300

    Book Like Mutation - Created the like update mutation and add error handling

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,12 @@ import path from "path";
 import { buildSchema } from "type-graphql";
 import { ApolloServer } from "apollo-server";
 import { LibraryResolver } from "./resolvers/LibraryResolver";
+import { BookResolver } from './resolvers/BookResolver';
 
 async function main() {
   const schema = await buildSchema({
     resolvers: [
+      BookResolver,
       LibraryResolver,
     ],
     emitSchemaFile: path.resolve(__dirname, 'schema.gql'),
